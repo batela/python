e22b20d096bf44f665777cd9f7fc36bba2b199cd
Borja <borjatellado@gmail.com>
2018-12-28 23:30:35 +0100

    Update mainpy.py

--- src/mainpy.py
+++ src/mainpy.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
 	# Esta seria la tercera linea de codigo
 	# Esta es la cuarta
 	# La quinta linea
-	# Sexta linea
+	# Esta sería las linea 5.1 añadida tras la seis.
 	
 	
     dtClasificator = CDayTypeClasificator.CDayTypeClasificator("")
